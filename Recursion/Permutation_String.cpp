54f89e9931d6fdf614e2345891955bbbfb21c285
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-02-06 18:14:26 +0530

    Add files via upload

--- Recursion/Permutation_String.cpp
+++ Recursion/Permutation_String.cpp
@@ -1,1 +1,22 @@
-
+void permute(vector<string> &ans,string s,int ind){
+	        if(ind==s.size()){
+	            ans.push_back(s);
+	            return;
+	        }
+	        
+	        for(int i=ind;i<s.size();i++){
+	            swap(s[ind],s[i]);
+	            permute(ans,s,ind+1);
+	            swap(s[ind],s[i]);
+	        }
+	        
+	    }
+	    
+		vector<string>find_permutation(string S)
+		{
+		    vector<string> ans;
+		    permute(ans,S,0);
+		    sort(ans.begin(),ans.end());
+		    ans.erase(unique(ans.begin(),ans.end()),ans.end());
+		    return ans;
+		}
