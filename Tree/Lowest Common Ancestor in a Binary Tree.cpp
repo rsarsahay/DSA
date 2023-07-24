8d532cdf2d4e5815d2800a7ab698f3108fa04216
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-07-24 12:15:23 +0530

    Update Lowest Common Ancestor in a Binary Tree.cpp

--- Tree/Lowest Common Ancestor in a Binary Tree.cpp
+++ Tree/Lowest Common Ancestor in a Binary Tree.cpp
@@ -3,20 +3,20 @@ class Solution
     public:
     //Function to return the lowest common ancestor in a Binary Tree.
     // T.C = O(n)
-    // S.C = O(1)
+    // S.C = O(n)
     Node* lca(Node* root ,int n1 ,int n2 )
     {
        //Your code here
+        //Base cases
        if(root == NULL)
        return root;
-       
        if(root->data == n1 || root->data == n2)
        return root;
        
        Node *left = lca(root->left , n1 , n2) ; 
        Node *right = lca(root->right , n1 , n2) ; 
        
-       if(left && right)
+       if(left && right)// if both right and left are not NULL we found result
            return root ; 
            
        if(left)
